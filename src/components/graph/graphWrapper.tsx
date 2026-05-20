5e4663e5f061536b3b6748f417e1df315f19813b
27bslash <greenpear68@yahoo.co.uk>
2026-05-20 16:44:11 +0100

    feat: added tooltip to graph showing difference from last day

--- src/components/graph/graphWrapper.tsx
+++ src/components/graph/graphWrapper.tsx
@@ -16,6 +16,7 @@ import useGetTrueBalance from './useGetTrueBalance';
 import GraphDialog from './graphDialog';
 import zoomPlugin from 'chartjs-plugin-zoom';
 import 'hammerjs';
+import { Box, Tooltip as MuiTooltip, Typography } from '@mui/material';
 export interface GraphDialogProps {
   open: boolean;
   setOpen: React.Dispatch<SetStateAction<boolean>>;
@@ -51,24 +52,40 @@ const GraphWrapper = ({ filter }: GraphProps) => {
   const { smarketsBalByDate, betfairBalByDate } = useGetTrueBalance(filter);
   useEffect(() => {
     const prevTrueBalance =
-      smarketsBalByDate[smarketsBalByDate.length - 2] +
-      betfairBalByDate[betfairBalByDate.length - 2];
+      smarketsBalByDate.at(-2)! + betfairBalByDate.at(-2)!;
     const currTrueBalance =
-      smarketsBalByDate[smarketsBalByDate.length - 1] +
-      betfairBalByDate[betfairBalByDate.length - 1];
-    console.log(currTrueBalance, prevTrueBalance);
+      smarketsBalByDate.at(-1)! + betfairBalByDate.at(-1)!;
     if (currTrueBalance < prevTrueBalance) {
       setNegative(true);
     }
   }, [smarketsBalByDate, betfairBalByDate]);
   return (
     <>
-      <TrendingUpIcon
-        onClick={() => setOpen((prev) => !prev)}
-        color={negative ? 'error' : 'success'}
-        className="icon"
-        sx={{ transform: negative ? 'rotate(60deg)' : 'none' }}
-      />
+      <MuiTooltip
+        title={
+          <Box display={'flex'}>
+            <Typography>Difference from previous day</Typography>
+            <Typography
+              color={negative ? 'error' : 'success'}
+              sx={{ fontWeight: 'bold',marginLeft:'5px' }}
+            >
+              £
+              {(
+                smarketsBalByDate.at(-1)! +
+                betfairBalByDate.at(-1)! -
+                (smarketsBalByDate.at(-2)! + betfairBalByDate.at(-2)!)
+              ).toFixed(2)}
+            </Typography>
+          </Box>
+        }
+      >
+        <TrendingUpIcon
+          onClick={() => setOpen((prev) => !prev)}
+          color={negative ? 'error' : 'success'}
+          className="icon"
+          sx={{ transform: negative ? 'rotate(60deg)' : 'none' }}
+        />
+      </MuiTooltip>
       {open && <GraphDialog filter={filter} open={open} setOpen={setOpen} />}
     </>
   );
